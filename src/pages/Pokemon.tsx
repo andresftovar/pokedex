86e91933eb91b0204672597e3e1a7f19be2f52c9
Andres Tovar <andrestovar12@outlook.com>
2023-04-24 19:00:45 -0500

    Finish project

---
+++ src/pages/Pokemon.tsx
@@ -0,0 +1,53 @@
+import { useNavigate, useParams} from "react-router-dom";
+import PokeballImg from "../assets/pokeball.png";
+import Footer from "../components/Footer";
+import styles from "./pokemon.module.css";
+import { PokemonDetails } from "../types/types";
+import { useEffect } from "react";
+import { useState } from "react";
+import { fetchPokemon } from "../api/fetchPokemon";
+import LoadingScreen from "../components/LoadingScreen";
+import { wait } from "../utils/utils";
+
+const Pokemon = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [pokemon, setPokemon] = useState<PokemonDetails>();
+    const {name} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getPokemon() {
+            setIsLoading(true);
+            await wait(200);
+            const fetchedPokemon = await fetchPokemon(name as string);
+            setPokemon(fetchedPokemon);
+            setIsLoading(false);
+        }
+        getPokemon();
+    }, [name]);
+
+    if (isLoading || !pokemon) return <LoadingScreen />
+
+    return (
+        <>
+        <button className={styles.pokeballButton} onClick={() => navigate(-1)}>
+            <img className={styles.pokeballImg} src={PokeballImg} alt="Pokeball" /> Go Back
+        </button>
+        <div className={styles.pokemon}>
+            <main className={styles.pokemonInfo}>
+                <div className={styles.pokemonTitle}>{pokemon?.name?.toUpperCase()}</div>
+                <div>Nr. {pokemon?.id}</div>
+                <div>
+                    <img className={styles.pokemonInfoImg} src={pokemon?.imgSrc} alt={pokemon?.name} />
+                </div>
+                <div>HP: {pokemon?.hp}</div>
+                <div>Attack: {pokemon?.attack}</div>
+                <div>Defense: {pokemon?.defense}</div>
+            </main>
+        </div>
+        <Footer />
+        </>
+    );
+};
+
+export default Pokemon;
